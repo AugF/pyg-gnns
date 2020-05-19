3fbb6998afa7dc35496387053177ef6d0a4f89fe
YP_Wang <wangyp@smail.nju.edu.cn>
2020-05-19 10:55:43 +0800

    add run.sh

---
+++ exp/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+conda activate pyg
+bash ./base_exp.sh
+bash ./config_exp.sh
+bash ./super_paras_exp.sh
+bash ./hidden_dims_exp.sh
+bash ./layers_exp.sh
+bash ./scalablity_exp.sh
