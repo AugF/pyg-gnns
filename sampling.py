33fdceadce3c5ebea5e80ba07fc201411c9a8ff8
AuF <wangyp@smail.nju.edu.cn>
2020-06-20 19:41:37 +0800

    add datasets and model

--- sampling.py
+++ sampling.py
@@ -1,19 +1,77 @@
 import torch
+import time
 import torch.nn.functional as F
 from torch.nn import ModuleList
 from tqdm import tqdm
 import argparse
 from torch_geometric.data import ClusterData, ClusterLoader, NeighborSampler
-from torch_geometric.nn import SAGEConv
-from utils import get_dataset
 
-dataset = get_dataset("reddit", normalize_features=True)
-data = dataset[0]
+import numpy as np
+import scipy.sparse as sp
+from gaan.models import GaAN
+from ggnn.models import GGNN
+from gat.models import GAT
+from gcn.models import GCN
+
+from utils import get_dataset, normalize, get_split_by_file, nvtx_push, nvtx_pop, log_memory, small_datasets
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, default='cora', help="dataset: [cora, flickr, com-amazon, reddit, com-lj,"
+                                                                    "amazon-computers, amazon-photo, coauthor-physics, pubmed]")
+parser.add_argument('--model', type=str, default='gat', help="gnn models: [gcn, ggnn, gat, gaan]")
+parser.add_argument('--epochs', type=int, default=2, help="epochs for training")
+parser.add_argument('--layers', type=int, default=2, help="layers for hidden layer")
+parser.add_argument('--hidden_dims', type=int, default=64, help="hidden layer output dims")
+parser.add_argument('--heads', type=int, default=8, help="gat or gaan model: heads")
+parser.add_argument('--head_dims', type=int, default=8, help="gat model: head dims") # head_dims * heads = hidden_dims
+parser.add_argument('--d_v', type=int, default=8, help="gaan model: vertex's dim") # d_v * heads = hidden_dims?
+parser.add_argument('--d_a', type=int, default=8, help="gaan model: each vertex's dim in edge attention") # d_a = head_dims
+parser.add_argument('--d_m', type=int, default=64, help="gaan model: gate: max aggregator's dim, default=64")
+
+parser.add_argument('--seed', type=int, default=1, help="random seed")
+parser.add_argument('--gpu', type=int, default=0, help='cpu:-1, cuda_id')
+parser.add_argument('--lr', type=float, default=0.01, help="adam's learning rate")
+parser.add_argument('--weight_decay', type=float, default=0.0005, help="adam's weight decay")
+parser.add_argument('--no_record_shapes', action='store_false', default=True, help="nvtx or autograd's profile to record shape")
+parser.add_argument('--json_path', type=str, default='', help="json file path for memory")
+parser.add_argument('--mode', type=str, default='cluster', help='sampling: [cluster, graphsage]')
+parser.add_argument('--batch_size', type=int, default=512, help='batch size')
+parser.add_argument('--batch_partitions', type=int, default=1, help='number of cluster partitions per batch')
+parser.add_argument('--cluster_partitions', type=int, default=500, help='number of cluster partitions')
 parser.add_argument('--num_workers', type=int, default=12, help='number of Data Loader partitions')
 args = parser.parse_args()
 
+gpu = args.gpu >= 0 and torch.cuda.is_available()
+flag = not args.json_path == ''
+
+print(args)
+print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+
+# 0. set manual seed
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if gpu:
+    torch.cuda.manual_seed(args.seed)
+
+# 1. set datasets
+dataset_info = args.dataset.split('_')
+if dataset_info[0] in small_datasets and len(dataset_info) > 1:
+    args.dataset = dataset_info[0]
+
+dataset = get_dataset(args.dataset, normalize_features=True)
+data = dataset[0]
+
+# add train, val, test split
+if args.dataset in ['amazon-computers', 'amazon-photo', 'coauthor-physics']:
+    file_path = osp.join("/data/wangzhaokang/wangyunpan", "data/" + args.dataset + "/raw/role.json")
+    data.train_mask, data.val_mask, data.test_mask = get_split_by_file(file_path, data.num_nodes)
+
+num_features = dataset.num_features
+if dataset_info[0] in small_datasets and len(dataset_info) > 1:
+    file_path = osp.join("/data/wangzhaokang/wangyunpan", "data/feats_x/" + '_'.join(dataset_info) + '_feats.npy')
+    if osp.exists(file_path):
+        data.x = torch.from_numpy(np.load(file_path)).to(torch.float) # 因为这里是随机生成的，不考虑normal features
+        num_features = data.x.size(1)
 
 cluster_data = ClusterData(data, num_parts=1500, recursive=False,
                            save_dir=dataset.processed_dir)
@@ -24,50 +82,41 @@ subgraph_loader = NeighborSampler(data.edge_index, sizes=[-1], batch_size=1024,
                                   shuffle=False, num_workers=args.num_workers)
 
 
-class Net(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(Net, self).__init__()
-        self.convs = ModuleList(
-            [SAGEConv(in_channels, 128),
-             SAGEConv(128, out_channels)])
-
-    def forward(self, x, edge_index):
-        for i, conv in enumerate(self.convs):
-            x = conv(x, edge_index)
-            if i != len(self.convs) - 1:
-                x = F.relu(x)
-                x = F.dropout(x, p=0.5, training=self.training)
-        return F.log_softmax(x, dim=-1)
-
-    def inference(self, x_all):
-        pbar = tqdm(total=x_all.size(0) * len(self.convs))
-        pbar.set_description('Evaluating')
-
-        # Compute representations of nodes layer by layer, using *all*
-        # available edges. This leads to faster computation in contrast to
-        # immediately computing the final representations of each batch.
-        for i, conv in enumerate(self.convs):
-            xs = []
-            for batch_size, n_id, adj in subgraph_loader:
-                edge_index, _, size = adj.to(device)
-                x = x_all[n_id].to(device)
-                x_target = x[:size[1]]
-                x = conv((x, x_target), edge_index)
-                if i != len(self.convs) - 1:
-                    x = F.relu(x)
-                xs.append(x.cpu())
-
-                pbar.update(batch_size)
-
-            x_all = torch.cat(xs, dim=0)
-
-        pbar.close()
-
-        return x_all
-
-
+if args.model == 'gcn':
+    # 预先计算edge_weight出来
+    adj = sp.coo_matrix((np.ones(data.edge_index.shape[1]), (data.edge_index[0, :], data.edge_index[1, :])),
+                        shape=(data.x.shape[0], data.x.shape[0]),
+                        dtype=np.float32)
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+    adj = normalize(adj + sp.eye(adj.shape[0]))
+    model = GCN(
+        layers=args.layers,
+        n_features=num_features, n_classes=dataset.num_classes,
+        hidden_dims=args.hidden_dims, gpu=gpu, flag=flag, adj=adj
+    )
+elif args.model == 'gat':
+    model = GAT(
+        layers=args.layers,
+        n_features=num_features, n_classes=dataset.num_classes,
+        head_dims=args.head_dims, heads=args.heads, gpu=gpu, flag=flag
+    )
+elif args.model == 'ggnn':
+    model = GGNN(
+        layers=args.layers,
+        n_features=num_features, n_classes=dataset.num_classes,
+        hidden_dims=args.hidden_dims, gpu=gpu, flag=flag
+    )
+elif args.model == 'gaan':
+    model = GaAN(
+        layers=args.layers,
+        n_features=num_features, n_classes=dataset.num_classes,
+        hidden_dims=args.hidden_dims,
+        heads=args.heads, d_v=args.d_v,
+        d_a=args.d_a, d_m=args.d_m, gpu=gpu, flag=flag
+    )
+    
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = Net(dataset.num_features, dataset.num_classes).to(device)
+model = model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 
 
