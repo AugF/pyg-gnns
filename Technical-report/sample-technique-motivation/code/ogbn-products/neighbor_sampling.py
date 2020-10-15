19541ada97f6722064a8dfe6e0512c40ad4f1f1f
AugF <wangyp@smail.nju.edu.cn>
2020-10-15 23:06:45 +0800

    增加了argparse库的包装

--- Technical-report/sample-technique-motivation/code/ogbn-products/neighbor_sampling.py
+++ Technical-report/sample-technique-motivation/code/ogbn-products/neighbor_sampling.py
@@ -1,6 +1,7 @@
 # Reaches around 0.7870 ± 0.0036 test accuracy.
 
 import os.path as osp
+import argparse
 
 import torch
 import torch.nn.functional as F
@@ -9,15 +10,29 @@ from ogb.nodeproppred import PygNodePropPredDataset, Evaluator
 from torch_geometric.data import NeighborSampler
 from torch_geometric.nn import SAGEConv
 
-# root = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'products')
+from logger import Logger
+
+parser = argparse.ArgumentParser(description='OGBN-MAG (SAGE)')
+parser.add_argument('--device', type=int, default=0)
+parser.add_argument('--num_layers', type=int, default=3)
+parser.add_argument('--hidden_channels', type=int, default=256)
+parser.add_argument('--batch_size', type=int, default=1024)
+parser.add_argument('--lr', type=float, default=0.003)
+parser.add_argument('--epochs', type=int, default=20)
+parser.add_argument('--runs', type=int, default=10)
+args = parser.parse_args()
+print(args)
+
 dataset = PygNodePropPredDataset('ogbn-products')
 split_idx = dataset.get_idx_split()
 evaluator = Evaluator(name='ogbn-products')
+logger = Logger(args.runs, args)
+
 data = dataset[0]
 
 train_idx = split_idx['train']
 train_loader = NeighborSampler(data.edge_index, node_idx=train_idx,
-                               sizes=[15, 10, 5], batch_size=1024,
+                               sizes=[15, 10, 5], batch_size=args.batch_size,
                                shuffle=True, num_workers=12)
 subgraph_loader = NeighborSampler(data.edge_index, node_idx=None, sizes=[-1],
                                   batch_size=4096, shuffle=False,
@@ -84,8 +99,8 @@ class SAGE(torch.nn.Module):
         return x_all
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = SAGE(dataset.num_features, 256, dataset.num_classes, num_layers=3)
+device = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
+model = SAGE(dataset.num_features, args.hidden_channels, dataset.num_classes, num_layers=args.num_layers)
 model = model.to(device)
 
 x = data.x.to(device)
@@ -98,7 +113,7 @@ def train(epoch):
     pbar = tqdm(total=train_idx.size(0))
     pbar.set_description(f'Epoch {epoch:02d}')
 
-    total_loss = total_correct = 0
+    total_loss = 0
     for batch_size, n_id, adjs in train_loader:
         # `adjs` holds a list of `(edge_index, e_id, size)` tuples.
         adjs = [adj.to(device) for adj in adjs]
@@ -110,15 +125,13 @@ def train(epoch):
         optimizer.step()
 
         total_loss += float(loss)
-        total_correct += int(out.argmax(dim=-1).eq(y[n_id[:batch_size]]).sum())
         pbar.update(batch_size)
 
     pbar.close()
 
     loss = total_loss / len(train_loader)
-    approx_acc = total_correct / train_idx.size(0)
 
-    return loss, approx_acc
+    return loss
 
 
 @torch.no_grad()
@@ -146,30 +159,21 @@ def test():
     return train_acc, val_acc, test_acc
 
 
-test_accs = []
-for run in range(1, 11):
-    print('')
-    print(f'Run {run:02d}:')
-    print('')
-
+test() # Test if inference on GPU succeeds.
+for run in range(args.runs):
     model.reset_parameters()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
-    best_val_acc = final_test_acc = 0
-    for epoch in range(1, 21):
-        loss, acc = train(epoch)
-        print(f'Epoch {epoch:02d}, Loss: {loss:.4f}, Approx. Train: {acc:.4f}')
-
-        if epoch > 5:
-            train_acc, val_acc, test_acc = test()
-            print(f'Train: {train_acc:.4f}, Val: {val_acc:.4f}, '
-                  f'Test: {test_acc:.4f}')
-
-            if val_acc > best_val_acc:
-                best_val_acc = val_acc
-                final_test_acc = test_acc
-    test_accs.append(final_test_acc)
-
-test_acc = torch.tensor(test_accs)
-print('============================')
-print(f'Final Test: {test_acc.mean():.4f} ± {test_acc.std():.4f}')
+    for epoch in range(1, 1 + args.epochs):
+        loss = train(epoch)
+        result = test()
+        logger.add_result(run, result)
+        train_acc, valid_acc, test_acc = result
+        print(f'Run: {run + 1:02d}, '
+              f'Epoch: {epoch:02d}, '
+              f'Loss: {loss:.4f}, '
+              f'Train: {100 * train_acc:.2f}%, '
+              f'Valid: {100 * valid_acc:.2f}%, '
+              f'Test: {100 * test_acc:.2f}%')
+    logger.print_statistics(run)
+logger.print_statistics()
